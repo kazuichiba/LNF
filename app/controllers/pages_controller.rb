a60c960d8acd7a0d6521e83398cb415c107b2f92
Kazuki Ichiba <Kazu.Ichiba@Gmail.com>
2013-06-12 16:16:32 -0400

    completed sign up on index page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,26 @@
 class PagesController < ApplicationController
+  helper_method :resource, :resource_name, :devise_mapping
+
   def index
+
   end
 
   def dashboard
   end
+  
+
+  protected
+  def resource
+    @resource ||= User.new
+  end
+
+  def resource_name
+    :user
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
+
 end
