6a89fdc36eac9259c3155f60c67ab614a9e84f10
Kazuki Ichiba <Kazu.Ichiba@Gmail.com>
2013-06-12 12:21:44 -0400

    heroku

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,15 +1,15 @@
-$('#myTab a').click(function (e) {
-  e.preventDefault();
-  $(this).tab('show');
-})
-$('#myTab a[href="#profile"]').tab('show'); // Select tab by name
-$('#myTab a:first').tab('show'); // Select first tab
-$('#myTab a:last').tab('show'); // Select last tab
-$('#myTab li:eq(2) a').tab('show'); // Select third tab (0-indexed)
+// $('#myTab a').click(function (e) {
+//   e.preventDefault();
+//   $(this).tab('show');
+// })
+// $('#myTab a[href="#profile"]').tab('show'); // Select tab by name
+// $('#myTab a:first').tab('show'); // Select first tab
+// $('#myTab a:last').tab('show'); // Select last tab
+// $('#myTab li:eq(2) a').tab('show'); // Select third tab (0-indexed)
 
 
 
 
 
 
-$('#myTab a[href="#back log"]').tab('show'); // Select tab by nameƒÏ`
+// $('#myTab a[href="#back log"]').tab('show'); // Select tab by nameƒÏ`
