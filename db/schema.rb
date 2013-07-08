9a03358d84e288487dd9ed049afa4218404f8ab3
Kazuki Ichiba <Kazu.Ichiba@Gmail.com>
2013-07-08 17:26:52 -0400

    cleaned up some html

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20130707230841) do
 
   create_table "comments", :force => true do |t|
     t.integer  "user_id"
     t.integer  "task_id"
     t.text     "body"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "posts", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
